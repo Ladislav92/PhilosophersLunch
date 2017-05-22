53ae76f6bf31b75826d3d2b5bba52f5d8328b290
Ladislav92 <milunovic.ladislav92@gmail.com>
2017-05-22 20:04:18 +0300

    Added Philosopher class, solved problem another way.

--- src/com/ladislav/Main.java
+++ src/com/ladislav/Main.java
@@ -6,98 +6,68 @@ package com.ladislav;
 public class Main {
 
     public static void main(String[] args) {
-
         Fork fork1 = new Fork();
         Fork fork2 = new Fork();
         Fork fork3 = new Fork();
         Fork fork4 = new Fork();
 
-        Thread diogenes = new Thread(() -> {
+        Philosopher heraclitus = new Philosopher("Heraclitus", fork1, fork2);
+        Philosopher socrates = new Philosopher("Socrates", fork2, fork1);
+        Philosopher diogenes = new Philosopher("Diogenes", fork3, fork4);
+        Philosopher plato = new Philosopher("Plato", fork4, fork1);
 
-            synchronized (fork1) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (fork2) {
-    
-                    fork1.eat();
-                    fork2.eat();
-                    System.out.println("Diogenes ate his macaroni.");
-
-                }
-            }
-        });
-
-        Thread heraclitus = new Thread(() -> {
-
-            synchronized (fork1) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (fork4) {
-
-                    fork1.eat();
-                    fork4.eat();
-                    System.out.println("Heraclitus ate his macaroni.");
-                }
-            }
-        });
-
-        Thread plato = new Thread(() -> {
-
-            synchronized (fork3) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (fork2) {
-
-                    fork3.eat();
-                    fork2.eat();
-                    System.out.println("Plato ate his macaroni.");
-                }
-            }
-        });
-
-        Thread socrates = new Thread(() -> {
-
-            synchronized (fork3) {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (fork4) {
-
-                    fork3.eat();
-                    fork4.eat();
-                    System.out.println("Socrates ate his macaroni.");
-                }
-            }
-        });
-
-        diogenes.start();
         heraclitus.start();
         socrates.start();
+        diogenes.start();
         plato.start();
-
     }
+
 }
 
+class Philosopher extends Thread {
+
+    private static final Object lock = new Object();
+    private final Fork left;
+    private final Fork right;
+
+    public Philosopher(String name, Fork left, Fork right) {
+        super(name);
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (lock) {
+            synchronized (left) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (right) {
+                    left.eat();
+                    right.eat();
+                    System.out.println(Thread.currentThread().getName() + " ate his macaroni.");
+                }
+            }
+        }
+    }
+}
 
 class Fork {
 
     public void eat() {
         try {
             Thread.sleep(50);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
 }
+
+
